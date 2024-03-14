469e9cb7e8846fd9532c93632860bd7572a2f520
Kirill-Fokin <kirill19011998@mail.ru>
2024-03-14 16:42:31 +0300

    feat: add blur and orices cards

---
+++ src/js/data.js
@@ -0,0 +1,14 @@
+export const PRICES = [    {
+       text :  'Release of Letraset sheets containing Lorem Ipsum passages, and more recently',
+       price : 25,
+    }, {
+        text :  'Release of Letraset sheets containing Lorem Ipsum passages, and more recently',
+        price : 35,
+    },
+    {
+        text :  'Release of Letraset sheets containing Lorem Ipsum passages, and more recently',
+        price : 15,
+    }
+    
+
+]
